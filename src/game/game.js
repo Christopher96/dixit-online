20cf91b31d8451b99835ec1d128db1273a83cb39
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-11-11 15:32:40 +0100

    game stuff

--- src/game/game.js
+++ src/game/game.js
@@ -1,48 +1,87 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
-import "./game.css";
+import "./game.css"
 
 class Game extends Component {
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
             game: null,
             status: "LOADING"
-        };
+        }
     }
 
     componentDidMount() {
-        this.getGame();
+        this.getGame()
     }
 
     getGame = () => {
         this.props.model
             .getGame(this.props.gameid)
-            .then((game) => this.setState({
-                game,
-                status: "LOADED"
-            }))
-            .catch(this.setState({
-                status: "ERROR"
-            }));
+            .then(res => {
+                if(!res.error) {
+                    this.setState({
+                        game: res,
+                        status: "LOADED"
+                    })
+                } else {
+                    this.setState({
+                        error: res.error,
+                        status: "ERROR"
+                    })
+                }
+            })
     }
 
     render() {
-        this.content = null;
+        let content = null
+
+        let { game } = this.state;
 
         switch(this.state.status) {
             case "LOADED":
-                break;
+                let players = game.players.map((player, i) => {
+                    let isCurrent = game.currentPlayer == i;
+                    return 
+                        <tr key={i} className={isCurrent ? 'current' : ''}>
+                            <td>{player.name}</td>
+                            <td>{player.score}</td>
+                        </tr>
+                })
+                content = 
+                    <div id="game">
+                        <p className="gameName">{this.props.gameid}</p>
+                        <div className="sidebar">
+                            <table className="players">
+                                <tr>
+                                    <th>Player</th>
+                                    <th>Score</th>
+                                </tr>
+                                {players}
+                            </table>
+                        </div>
+                        <div className="field">
+                        </div>
+                    </div>
+                break
+            case "LOADING":
+                content = 
+                    <div className="center">
+                        <div className="loader"></div> 
+                    </div>
+                break
+            case "ERROR":
+                content = 
+                    <div className="center">
+                        <p className="notFound">404: {this.state.error}</p>
+                    </div>
+                break
         }
 
-        return (
-            <div className="Welcome">
-                <p>{this.props.gameid}</p>
-            </div>
-        );
+        return content
     }
 }
 
-export default Game;
+export default Game
