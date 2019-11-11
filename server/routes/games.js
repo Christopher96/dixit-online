8559d0f9f490052ff5ca808aaee5338b8764496d
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-11-11 21:53:16 +0100

    undefined 'game', fixed

--- server/routes/games.js
+++ server/routes/games.js
@@ -53,21 +53,6 @@ router.post('/save', (req, res, next) => {
 
     Game.updateOne({ _id: doc._id }, { $set: game })
         .then((game) => {
-            if(game != null) {
-                return res.status(403).send(`Game with ID '${gameid}' already exists.`)
-            } else {
-                let playerObjects = players.map(name => new Player({name}));
-                let game = new Game({ gameid })
-                game.players = playerObjects
-
-                game.save(function (err) {
-                    if (err) {
-                        return res.status(500).send(err)
-                    } else {
-                        return res.status(200).json(game)
-                    }
-                })
-            }
         })
         .catch((e) => {
             return res.status(500).send(e)
