18d5f09ca94715dbf3c7a6cd2af56813464930db
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-11-12 18:39:12 +0100

    added procfile and build script for heroku

--- server/index.js
+++ server/index.js
@@ -39,6 +39,14 @@ router.use('/images', require('./routes/images'))
 app.use('/test', require('./routes/populate'))
 app.use('/api', router)
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'../build/index.html'));
+});
+
 // Listen the server
 app.listen(port, host)
 app.on('listening', function() {
